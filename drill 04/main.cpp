3162385ddfae07c26200ce4aa4a40b3738984703
levaibence <levaibence@gmail.com>
2022-03-16 11:19:56 +0100

    asd x

---
+++ drill 04/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Graph.h"
+#include "Window.h"
+
+int main()
+{
+    int wwidth = 600;
+    int wheight = 600;
+    Fl_Window win{wwidth,wheight,"Function graphs."};
+    
+    return 0;
+}
